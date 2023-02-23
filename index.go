a8e71fc214a651a9fc58ebf772059d1fda119cf3
Karitham <kar@karitham.dev>
2023-02-23 10:34:12 +0100

    responsive

--- index.go
+++ index.go
@@ -4,15 +4,24 @@ import (
 	"embed"
 	"html/template"
 	"net/http"
+	"os"
+	"sync"
 )
 
 //go:embed front/*
 var frontFS embed.FS
 
 var idxT = template.Must(template.New("index.html").ParseFS(frontFS, "front/index.html"))
+var idxTMu = sync.Mutex{}
 
 func (s *server) index(w http.ResponseWriter, r *http.Request) {
 	links, _ := s.store.List(user)
+
+	if os.Getenv("DEBUG") != "" {
+		idxTMu.Lock()
+		idxT = template.Must(template.ParseFiles("front/index.html"))
+		defer idxTMu.Unlock()
+	}
 
 	w.Header().Set("Content-Type", "text/html")
 	if err := idxT.Execute(w, links); err != nil {
